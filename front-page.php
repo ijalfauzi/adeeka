88fc542ac1512efa02d3a86d613fd3bf106afba0
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-01-18 15:54:18 +0700

    Added front-page.php

---
+++ front-page.php
@@ -0,0 +1,157 @@
+<?php
+
+    get_header(); 
+
+    ?>
+
+    <div class="large-hero page-section">
+        <div class="wrapper">
+            <div class="row row--gutters">
+                <div class="row__medium-12 text-right">
+                    <h1 class="large-hero__title">Your Smart <br>Water Treatment <br>Solution</h1>
+                    <p class="large-hero__description">We treat water for a better future</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="site-service page-section">
+        <div class="wrapper">
+            <h2 class="headline headline--centered">Our Services</h2>
+            <div class="row row--gutters-small">
+                <div class="row__medium-4">
+                    <a title="Sewage Treatment Plant" href="/stp">
+                        <div class="service">
+                            <div class="service__icon">
+                                <img src="<?php echo get_theme_file_uri('assets/images/sewage-treatment-plant.webp') ?>" alt="Sewage Treatment Plant">
+                            </div>
+                            <h3 class="service__title">Sewage Treatment Plant</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="row__medium-4">
+                    <a title="Water Treatment Plant" href="/wtp">
+                        <div class="service">
+                            <div class="service__icon">
+                                <img src="<?php echo get_theme_file_uri('assets/images/water-treatment-plant.webp') ?>" alt="Water Treatment Plant">
+                            </div>
+                            <h3 class="service__title">Water Treatment Plant</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="row__medium-4">
+                    <a title="Desalinasi" href="/desalinasi">
+                        <div class="service">
+                            <div class="service__icon">
+                                <img src="<?php echo get_theme_file_uri('assets/images/desalinasi.webp') ?>" alt="Desalinasi">
+                            </div>
+                            <h3 class="service__title">Desalinasi</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-application page-section">
+        <div class="wrapper">
+            <div class="row row--gutters">
+                <div class="row__medium-5">
+                    <div class="featured-media">
+                        <picture>
+                            <source srcset="<?php echo get_theme_file_uri('assets/images/atd-home--desktop.webp 1010w') ?>" media="(min-width: 1010px)">
+                            <source srcset="<?php echo get_theme_file_uri('assets/images/atd-home--tablet.webp 767w') ?>" media="(min-width: 767px)">
+                            <img srcset="<?php echo get_theme_file_uri('assets/images/atd-home--smartphone.webp 530w') ?>" alt="PT Adika Tirta Daya">
+                        </picture>
+                    </div>
+                </div>
+                <div class="row__medium-7">
+                    <h2>Persediaan air semakin menipis. Mari kita selamatkan!</h2>
+                    <p>Pengolahan air limbah menjadi salah satu cara untuk menyelamatkan persediaan air di bumi. PT Adika Tirta Daya membantu Anda mewujudkan pengolahan air limbang yang efektif dan efisien.</p>
+                    <p><strong>Melayani dengan cara terbaik kami:</strong>
+                        <ul class="text-left">
+                            <li><a href="/stp" title="Sewage Treatment Plant">Sewage Treatment Plant</a>, <a href="/wtp" title="Water Treatment Plant">Water Treatment Plant</a>, <a href="/desalinasi" title="Desalinasi">Desalinasi</a></li>
+                            <li>Menggunakan metode aerasi dan filtrasi</li>
+                            <li>Dapat diterapkan di <a href="/industri" title="Industri">berbagai sektor industri</a></li>
+                            <li>Mengutamakan kualitas, komitmen dan inovasi</li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-application page-section site-application--green">
+        <div class="wrapper">
+            <div class="row row--gutters">
+                <div class="row__medium-7">
+                    <h2>Mengapa harus memilih Adika Tirta Daya?</h2>
+                    <ul class="text-left">
+                        <li>Anda cukup berinvestasi, kami yang menyiapkan desain IPAL, peralatan dan pengoperasian sistemnya</li>
+                        <li>Mengikuti standar baku mutu sesuai dengan aturan pemerintah</li>
+                        <li>Memiliki operator sistem selama 24 jam</li>
+                        <li><a href="/layanan" title="Pay for Performance">Pay for Performance</a>, tentu akan menghemat biaya
+                        </li>
+                    </ul>
+                    <p><strong>Segera hubungi tim kami: <a href="https://api.whatsapp.com/send?phone=6282116612013" title="Hubungi Kami">Your Smart Water Treatment Solution!</a></strong></p>    
+                </div>
+                <div class="row__medium-5">
+                    <div class="featured-image">
+                        <picture>
+                            <source srcset="<?php echo get_theme_file_uri('assets/images/atd-featured--desktop.webp 1010w') ?>" media="(min-width: 1010px)">
+                            <source srcset="<?php echo get_theme_file_uri('assets/images/atd-featured--desktop.webp 767w') ?>" media="(min-width: 767px)">
+                            <img srcset="<?php echo get_theme_file_uri('assets/images/atd-featured--smartphone.webp 530w') ?>" alt="PT Adika Tirta Daya">
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-project page-section">
+        <div class="wrapper">
+            <h2 class="headline headline--centered headline--capitalized">Proudly Displaying Projects</h2>
+            <div class="row row--gutters-small">
+                <div class="row__medium-4">
+                    <a href="/emporium" title="Emporium Mall"><img src="<?php echo get_theme_file_uri('assets/images/emporium-mall.webp'); ?>" alt="Emporium Mall"></a>
+                </div>
+                <div class="row__medium-4">
+                    <a href="/moi" title="Mall of Indonesia"><img src="<?php echo get_theme_file_uri('assets/images/mall-of-indonesia.webp'); ?>" alt="Mall of Indonesia"></a>
+                </div>
+                <div class="row__medium-4">
+                    <a href="/pejaten-village" title="Pejaten Village"><img src="<?php echo get_theme_file_uri('assets/images/pejaten-village-mall.webp'); ?>" alt="Pejaten Village Mall"></a>
+                </div>
+            </div>
+            <div class="row row--gutters-small">
+                <div class="row__medium-4">
+                    <a href="/cbd" title="Apartemen CBD Pluit"><img src="<?php echo get_theme_file_uri('assets/images/apartemen-cbd-pluit.webp'); ?>" alt="Apartemen CBD Pluit"></a>
+                </div>
+                <div class="row__medium-4">
+                    <a href="/frenchwalk" title="Apartemen Frenchwalk"><img src="<?php echo get_theme_file_uri('assets/images/apartemen-frenchwalk.webp'); ?>" alt="Apartemen Frencwalk"></a>
+                </div>
+                <div class="row__medium-4">
+                    <a href="/redtop" title="Hotel Red Top"><img src="<?php echo get_theme_file_uri('assets/images/hotel-red-top.webp'); ?>" alt="Hotel Red Top"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-client page-section">
+        <div class="wrapper">
+            <h2 class="headline">Our Clients</h2>
+            <div class="row">
+                <div class="row__medium-12">
+                    <div class="partner">
+                        <picture>
+                            <img src="<?php echo get_theme_file_uri('assets/images/atd-clients-20230116.webp'); ?>" alt="Klien PT Adika Tirta Daya">
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php 
+
+    get_footer();
+
+?>
