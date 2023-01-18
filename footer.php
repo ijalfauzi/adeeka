1640f71a5472963f019951a79f253646acc4c906
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-01-18 16:49:41 +0700

    Updated footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,64 @@
+        <div class="site-widget page-section page-section--grey">
+            <div class="wrapper">
+                <div class="row row--gutters-small">
+                    <div class="row__large-6">
+                        <div class="widget-1">
+                            <div class="widget-content">
+                                <img src="<?php echo get_theme_file_uri('/assets/images/atd-footer.webp'); ?>" alt="PT Adika Tirta Daya">
+                            </div>
+                            <div class="widget-content">
+                                <h3 class="haedline headline--widget">Follow Us</h3>
+                                <div class="site-widget__socials">
+                                    <a title="Linkedin" href="https://www.linkedin.com/company/adika-tirta-daya/"><img src="<?php echo get_theme_file_uri('/assets/images/icons/linkedin.webp'); ?>" alt="Linkedin"></a>
+                                    <a title="Instagram" href="https://www.instagram.com/adikatirtadaya_/"><img src="<?php echo get_theme_file_uri('/assets/images/icons/instagram.webp'); ?>" alt="Instagram"></a>
+                                    <a title="Facebook" href="https://www.facebook.com/adiktirtadaya/"><img src="<?php echo get_theme_file_uri('/assets/images/icons/facebook.webp'); ?>" alt="Facebook"></a>
+                                    <a title="Youtube" href="https://www.youtube.com/channel/UCXKPdXfIRLODfOs8qykL1Mg"><img src="<?php echo get_theme_file_uri('/assets/images/icons/youtube.webp'); ?>" alt="Youtube"></a>
+                                    <a title="Twitter" href="https://twitter.com/adikatirtadaya"><img src="<?php echo get_theme_file_uri('/assets/images/icons/twitter.webp'); ?>" alt="Twitter"></a>
+                                </div>
+                                <p><a title="Download Company Profile" href="<?php echo site_url('download-company-profile') ?>" class="btn btn--dark"><img src="<?php echo get_theme_file_uri('/assets/images/icons/download.webp'); ?>" alt="Download Company Profile"> Download Company Profile</a></p>
+                                <p class="pt-1">Certified by:</p>
+                                <p><img src="<?php echo get_theme_file_uri('/assets/images/tuv-rheinland.webp'); ?>" alt="Tuv Rheinland Certified"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row__large-6">
+                        <div class="widget-3">
+                            <div class="widget-content widget-content--last-widget">
+                                <h3 class="haedline headline--widget">Contact Us</h3>
+                                <ul>
+                                    <li class="address">
+                                        <a title="Temukan di Google Maps" href="https://g.page/pt-adika-tirta-daya?share">
+                                            <img src="<?php echo get_theme_file_uri('/assets/images/icons/map.webp'); ?>" alt="Alamat">
+                                            <div class="text-adress">
+                                                Jl. Margasatwa Raya No.111, RT.2/RW.5, Pd. Labu, Kec. Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12450
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="phone">
+                                        <a title="Telepon Kami" href="tel:622129125884">
+                                            <img src="<?php echo get_theme_file_uri('/assets/images/icons/phone.webp'); ?>" alt="Telepon">
+                                            <div class="text-phone">Telp: +62-21-29125884</div>
+                                        </a>
+                                    </li>
+                                    <li class="phone">
+                                        <a title="Telepon Kami" href="https://api.whatsapp.com/send?phone=6282116612013">
+                                            <img src="<?php echo get_theme_file_uri('/assets/images/icons/whatsapp.webp'); ?>" alt="Telepon">
+                                            <div class="text-phone">WA: +62-821-1661-2013</div>
+                                        </a>
+                                    </li>
+                                    <li class="email">
+                                        <a title="Email Kami" href="mailto:marketing@adikatirtadaya.co.id">
+                                            <img src="<?php echo get_theme_file_uri('/assets/images/icons/envelope.webp'); ?>" alt="Email">
+                                            <div class="text-email">Email: marketing@adikatirtadaya.co.id</div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <footer class="site-footer">
             <div class="wrapper">
                 <p class="site-footer__text">
