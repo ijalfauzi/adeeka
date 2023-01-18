ddce47f490fed455e9b3722069ef813c7071cc33
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-01-18 14:54:42 +0700

    Updated footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
-    <?php wp_footer(); ?>
+        <footer class="site-footer">
+            <div class="wrapper">
+                <p class="site-footer__text">
+                    <p>© 2022 <a title="PT Adika Tirta Daya" href="<?php echo site_url(); ?>">PT Adika Tirta Daya</a><span>. All Rights Reserved.</span></p>
+                </p>
+            </div>
+        </footer>
+        <?php wp_footer(); ?>
     </body>
 </html>
