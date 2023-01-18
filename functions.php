37c2a14c4d6f07e41b8b9bd27eb48f9aecd8cf31
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-01-18 15:30:25 +0700

    Updated enqueue script syntax.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function adeeka_files() {
     wp_enqueue_style('adeeka_main_files', get_theme_file_uri('/assets/css/styles.css'));
-    wp_enqueue_script('adeeka_main_js', get_theme_file_uri('/assets/js/main.js'), false);
+    wp_enqueue_script('adeeka_main_js', get_theme_file_uri('/assets/js/main.js'), NULL, '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'adeeka_files');
