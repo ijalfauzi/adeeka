ce027dc6eaeba1820c2a11ecb37d22190dea37dc
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-01-18 14:19:02 +0700

    Loaded main styles.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function adeeka_files() {
-    wp_enqueue_style('adeeka_main_files', get_stylesheet_uri());
+    wp_enqueue_style('adeeka_main_files', get_theme_file_uri('/assets/css/styles.css'));
 }
 
 add_action('wp_enqueue_scripts', 'adeeka_files');
